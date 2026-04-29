4c5bd662020a6b6d6ce16e363ccae4bd98b409ea
RohYuri <yule026@gmail.com>
2026-04-29 11:51:23 +0900

    move 2

--- sketch.js
+++ sketch.js
@@ -15,10 +15,11 @@ function draw() {
   background(0);
   image(mapImg, 0, 0, 900, 500);
   
-  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) px -= 2;  // A
-  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) px += 2; // D
-  if (keyIsDown(UP_ARROW) || keyIsDown(87)) py -= 2;    // W
-  if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) py += 2;  // S
+  // WASD로 이동
+  if (keyIsDown(87)) py -= 2; // w
+  if (keyIsDown(83)) py += 2; // s
+  if (keyIsDown(65)) px -= 2; // a
+  if (keyIsDown(68)) px += 2; // d
   
   fill(255, 220, 0);
   noStroke();
