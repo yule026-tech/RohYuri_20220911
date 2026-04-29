d3ee38034818c4e7da391e6dc33454521b1be66a
RohYuri <yule026@gmail.com>
2026-04-29 11:00:59 +0900

    add character

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 var mapImg;
+var px, py;
 
 function preload() {
-  
   mapImg = loadImage('Map.png');
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 500);
+  px = 450;
+  py = 300;
 }
 
 function draw() {
   background(0);
-  image(mapImg, 0, 0, 400, 400);
+  image(mapImg, 0, 0, 900, 500);
+  
+  fill(255, 220, 0);
+  noStroke();
+  ellipse(px, py, 20, 20);
 }
