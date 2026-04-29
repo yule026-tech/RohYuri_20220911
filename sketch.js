334431ce795f7e8aa7e54a85f093ecfb6bb69664
RohYuri <yule026@gmail.com>
2026-04-29 10:39:44 +0900

    map image load

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
-function  setup() {
+var mapImg;
+
+function preload() {
+  
+  mapImg = loadImage('Map.png');
+}
+
+function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  image(mapImg, 0, 0, 400, 400);
 }
