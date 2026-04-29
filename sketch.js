6144605cc8b6bc7cab0404782791c51e0690c5fa
RohYuri <yule026@gmail.com>
2026-04-29 11:55:49 +0900

    화면 밖 제한

--- sketch.js
+++ sketch.js
@@ -15,11 +15,15 @@ function draw() {
   background(0);
   image(mapImg, 0, 0, 900, 500);
   
-  // WASD로 이동
-  if (keyIsDown(87)) py -= 2; // w
-  if (keyIsDown(83)) py += 2; // s
-  if (keyIsDown(65)) px -= 2; // a
-  if (keyIsDown(68)) px += 2; // d
+  // WASD 또는 방향키로 이동
+  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) px -= 2;  // A
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) px += 2; // D
+  if (keyIsDown(UP_ARROW) || keyIsDown(87)) py -= 2;    // W
+  if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) py += 2;  // S
+  
+  // 화면 밖 못나가게함
+  px = constrain(px, 0, 900);
+  py = constrain(py, 0, 500);
   
   fill(255, 220, 0);
   noStroke();
