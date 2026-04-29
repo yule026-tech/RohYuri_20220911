86a701e01b02b4357e1c48ee18f3fc03d68e48ef
RohYuri <yule026@gmail.com>
2026-04-29 11:57:52 +0900

    벽 충돌

--- sketch.js
+++ sketch.js
@@ -21,7 +21,13 @@ function draw() {
   if (keyIsDown(UP_ARROW) || keyIsDown(87)) py -= 2;    // W
   if (keyIsDown(DOWN_ARROW) || keyIsDown(83)) py += 2;  // S
   
-  // 화면 밖 못나가게함
+  // 벽 충돌 
+  var c = get(px, py);
+  if (c[2] > 100) { // 파란색이면 벽
+    px = 450;
+    py = 298;
+  }
+  
   px = constrain(px, 0, 900);
   py = constrain(py, 0, 500);
   
